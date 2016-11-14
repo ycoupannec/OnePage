1b161d9926c95d34893b349f33c74867088251fd
ycoupannec <yohann.coupannec@gmail.com>
2016-11-14 17:34:41 +0100

    maj Checkbox
    rectification du style de la checkbox
    l'emplacement n'est pas encore bon.

--- index.php
+++ index.php
@@ -98,10 +98,11 @@
 							<div class="onoffswitch">
 							    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked>
 							    <label class="onoffswitch-label" for="myonoffswitch">
-							        <span class="onoffswitch-inner"></span>люблю Россию
+							        <span class="onoffswitch-inner"></span>
 							        <span class="onoffswitch-switch"></span>
 							    </label>
 							</div>
+							люблю Россию
 							</br>
 							<button>послать</button>
 							
