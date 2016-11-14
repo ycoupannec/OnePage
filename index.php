7912b5b1fcb3ec58f0cb9889dc533821fbd1b5cd
ycoupannec <yohann.coupannec@gmail.com>
2016-11-14 11:44:43 +0100

    First Commit
    Ajout de Bootstrap

---
+++ index.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+	  <meta charset="UTF-8"/>
+	  <link href="css/bootstrap.min.css" rel="stylesheet">
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+	  <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/javas.js"></script>
+
+	  <title>Mock up</title>
+	</head>
+	<body id="top">
+
+
+		<header class="container-fluid">
+
+			<nav class="navbar">
+				<div class="container text-center">
+				  	<ul class=" col-md-12 list-inline ">
+					  	<li class="active"><a href="#">Mock Up</a></li>
+					  	<li><a href="#">Produit advantage</a></li>
+					  	<li><a href="#">They loved it</a></li>
+					  	<li><a href="#">Contact-us</a></li>
+				  	</ul>
+				</div>
+			</nav>
+		</header>
+
+
+		<main >
+			<section class="container text-center">
+				<article id="article1" class="row col-md-6">
+					<h2 >Still trying to reach ...</h2>
+					<h3>... your full potential ?</h3>
+					<p>
+						<<... so close ...>>
+						<<... and yet so far ...>>
+						<<... looks yummy ...>>
+						<<... goch it's harsh ...>>
+						<<... much difficult ...>>
+						<<... many powerfull ...>>
+						<<... my precious ...>>
+					</p>
+				</article>
+				<article id="article2" class="row">
+					<h2 class="col-md-12"> Try <i>Mock Up</i>, it taste like good ol' socialism and ...</h2>
+					<figure class="col-md-4">
+						<p>It's full of energy !</p>
+					</figure>
+					<figure class="col-md-4">
+						<p>full of vitamins * to *</p>
+					</figure>
+					<figure class="col-md-4">
+						<p>it's gluten free* !</p>
+						<p>*still, you might find some traces of arsenic ... sorry about that.</p>
+					</figure>
+				</article>
+				<article id="article3" class="row">
+					<figure class="col-md-4">
+						<p>
+							"BEEEUUUUUAAAAAAAAARRRRRRRGGGGGHHHHHHHHHHHHHHHHHH !!!!
+							GIMME MOCK-UP !!!"
+						</p>
+					</figure>
+					<figure class="col-md-4">
+						<p>"Woof ! woofwowoowooof !
+						wawawoowowoowof woof WOOF !!!"</p>
+					</figure>
+					<figure class="col-md-4">
+						"Seriously, don't eat that, it smells like shit ... wait ? Oh, alright then Yeah yeah it's great, please eat Mock-up plizz =]"
+					</figure>
+				</article>
+				<article id="article4" class="row">
+					<h2>Contact us ...</h2>
+					<p>Materialise pour le formulaire.</p>
+				</article>
+			</section>
+		</main>
+		<footer class="container">
+			<h2 class="col-md-12">MockUp</h2>
+			<ul class="col-md-6">
+				<li>Product advantage</li>
+				<li>They loved it</li>
+				<li>Contact-us</li>
+			</ul>
+			<ul class="col-md-6">
+				<li><a href="">Rutube</a></li>
+				<li><a href="">facebook</a></li>
+				<li><a href="">Pinterest</a></li>
+			</ul>
+		</footer>
+	</body>
+</html>
